368223733dceb3302f9611a5f582cc0caf86120c
Muhammadalif1213 <muhammadalif1213@gmail.com>
2023-03-24 15:22:45 +0700

    menambahkan array

--- belajar_insertion_sort/belajar_insertion_sort.cpp
+++ belajar_insertion_sort/belajar_insertion_sort.cpp
@@ -5,5 +5,15 @@ int arr[20];
 int n;
 
 void input() {
-	
-}
+	while (true) {
+		cout << "masukan jumlah data pada array";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+			cout << "\n Array yang anda masukan maksimal 20 elemen. \n";
+		}
+	}
